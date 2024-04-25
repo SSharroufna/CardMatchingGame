feedf50235801a99e80aa47d85eae54f9a4002dc
SarahSharroufna <sash1048@colorado.edu>
2024-04-25 15:44:53 -0600

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    // Setting up the graphics view and scene
+    QGraphicsView *view = new QGraphicsView(this);
+    scene = new QGraphicsScene(this);
+    view->setScene(scene);
+    setCentralWidget(view);
+
+    // Load card images and create pixmap items
+    // A QVector<QPixmap> containing card images
+    QVector<QPixmap> cardImages;
+
+    // Populate cardImages
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            int randomIndex = QRandomGenerator::global()->bounded(cardImages.size());
+            QPixmap cardPixmap = cardImages.takeAt(randomIndex);
+            QGraphicsPixmapItem *cardItem = new QGraphicsPixmapItem(cardPixmap);
+            cardItem->setPos(i * 100, j * 150); //Position in the UI
+            scene->addItem(cardItem);
+            cards.append(cardItem);
+        }
+    }
+
+    // Connect signals and slots
+    connect(scene, &QGraphicsScene::selectionChanged, this, &MainWindow::handleCardClick);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::handleCardClick()
+{
+    QList<QGraphicsItem*> selectedItems = scene->selectedItems();
+    if (selectedItems.size() == 2) {
+        QGraphicsPixmapItem *firstCard = qgraphicsitem_cast<QGraphicsPixmapItem*>(selectedItems.at(0));
+        QGraphicsPixmapItem *secondCard = qgraphicsitem_cast<QGraphicsPixmapItem*>(selectedItems.at(1));
+        if (firstCard && secondCard) {
+            // Check if cards match
+            if (firstCard->pixmap().toImage() == secondCard->pixmap().toImage()) {
+                qDebug() << "Match!";
+
+            } else {
+                qDebug() << "Not a match!";
+
+            }
+        }
+        scene->clearSelection();
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+#include <QRandomGenerator>
+#include <QVector>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void handleCardClick();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QVector<QGraphicsPixmapItem*> cards;
+};
+#endif // MAINWINDOW_H
