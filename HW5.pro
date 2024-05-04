82c465401729396e47dea12850b4b9ed201fdd1d
Lilia Garcia <liliagarcia0511@gmail.com>
2024-05-04 13:57:44 -0600

    Added resource file for images & changed path

--- HW5.pro
+++ HW5.pro
@@ -37,3 +37,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 DISTFILES +=
+
+RESOURCES += \
+    cardImages.qrc
--- cardprototype.cpp
+++ cardprototype.cpp
@@ -5,37 +5,37 @@
 Card* CardPrototypeFactory::createPrototype(CardType type) {
     switch (type) {
     case computer:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card13.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card13.png"), QPixmap(":/images/back.png"));
     case boy:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card1.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card1.png"), QPixmap(":/images/back.png"));
     case openBox:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card3.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap("/images/card3.png"), QPixmap(":/images/back.png"));
     case target:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card4.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card4.png"), QPixmap(":/images/back.png"));
     case map:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card5.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card5.png"), QPixmap(":/images/back.png"));
     case game:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card6.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card6.png"), QPixmap(":/images/back.png"));
     case clock:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card7.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card7.png"), QPixmap(":/images/back.png"));
     case speaker:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card2.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card2.png"), QPixmap(":/images/back.png"));
     case bag:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card12.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card12.png"), QPixmap(":/images/back.png"));
     case board:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card11.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card11.png"), QPixmap(":/images/back.png"));
     case piano:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card10.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card10.png"), QPixmap(":/images/back.png"));
     case closedBox:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card9.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card9.png"), QPixmap(":/images/back.png"));
     case gem:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/card8.png"), QPixmap("/Users/sarah/Downloads/images/back.png"));
+        return new Card(QPixmap(":/images/card8.png"), QPixmap(":/images/back.png"));
     case extraTime:
-        return new Card(QPixmap("/Users/sarah/Downloads/images/time.png"), QPixmap("/Users/sarah/Downloads/images/time.png"));
+        return new Card(QPixmap(":/images/time.png"), QPixmap(":/images/time.png"));
     case doublePoint:
-        return new DoublePointCard(QPixmap("/Users/sarah/Downloads/images/double.png"), QPixmap("/Users/sarah/Downloads/images/double.png"));
+        return new DoublePointCard(QPixmap(":/images/double.png"), QPixmap(":/images/double.png"));
     case glancer:
-        return new GlancerCard(QPixmap("/Users/sarah/Downloads/images/glancer.png"), QPixmap("/Users/sarah/Downloads/images/glancer.png"));
+        return new GlancerCard(QPixmap(":/images/glancer.png"), QPixmap(":/images/glancer.png"));
     default:
         return nullptr;
     }
